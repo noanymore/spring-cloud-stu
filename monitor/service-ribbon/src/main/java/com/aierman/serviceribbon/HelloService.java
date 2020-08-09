a23543122fd1be93f7d3cefb11ff954def6f6923
aierman <aierman@weierai.com>
2020-08-09 20:51:04 +0800

    阿西吧

---
+++ monitor/service-ribbon/src/main/java/com/aierman/serviceribbon/HelloService.java
@@ -0,0 +1,25 @@
+package com.aierman.serviceribbon;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return new String(name+"'s Hystrix起作用，熔断http://SERVICE-HI/hi的请求");
+    }
+
+
+}
+
---
+++ monitor/service-zuul/src/main/java/com/aierman/servicezuul/MyFilter.java
@@ -0,0 +1,53 @@
+package com.aierman.servicezuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+    @Override
+    //路由之前
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    //过滤的顺序
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    //写逻辑判断 是否要过滤
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    //过滤器的具体逻辑
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
